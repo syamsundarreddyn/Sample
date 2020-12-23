d2155533116d220a81ea4ffdaa5f3ad1e577cb87
syamsundarreddyn <narasyam999@gmail.com>
2020-12-23 10:44:45 +0530

    emp

--- Hello.py
+++ Hello.py
@@ -3,3 +3,16 @@ print("hello python")
 def ind(id = 101):
     print(id)
 ind()
+
+class Emp:
+    offi_add = 'bengalore'
+    def __init__(self, name, age, sal):
+        self.name = name
+        self.age = age
+        self.sal = sal
+    def get_emp_details(self):
+        print("Emp details are:",self.name,self.age,self.sal,self.offi_add)
+obj = Emp('sarath',23,234445)
+obj.get_emp_details()
+
+
